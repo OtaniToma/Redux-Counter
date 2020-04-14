1623f837db95c77c052a51a51fd24d29c1384b4f
Toma Otani <otani.toma@gmail.com>
2020-04-14 15:32:13 -0700

    connect redux from class component

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { NavLink as Link } from "react-router-dom";
+import { increment, decrement, plusten } from '../actions';
+import { connect } from 'react-redux';
 
-export default class extends React.Component {
+class About extends React.Component {
   constructor(props) {
-    super(props)
-
-    this.state = {
-    }
+    super(props);
   }
 
   render() {
@@ -14,8 +14,32 @@ export default class extends React.Component {
     return (
       <>
       <h1>About</h1>
+      <p>カウンター：{this.props.counter}</p>
+      <button onClick={this.props.increment}>+1</button>
+      <button onClick={this.props.decrement}>-1</button>
+      <button onClick={this.props.plusten}>+10</button>
+      <br />
+      <br />
       <Link to="./">Home</Link>
       </>
     )
   }
 }
+
+About.propTypes = {
+  count: PropTypes.number,
+  increment: PropTypes.func.isRequired,
+  decrement: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  counter: state.counter,
+});
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+  plusten: () => dispatch(plusten()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
